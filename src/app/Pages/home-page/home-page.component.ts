e68f721b528dbb0acfb8de24a2c1b6d260b12baa
detrujil1 <ingtrujillo6@gmail.com>
2024-10-21 22:14:09 -0500

    add inteface product

--- src/app/Pages/home-page/home-page.component.ts
+++ src/app/Pages/home-page/home-page.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { Product } from './ProductInterface';
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [HeaderComponent, FooterComponent, CardsComponent],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']  // Corrección aquí
+  styleUrls: ['./home-page.component.css'], // Corrección aquí
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  products: Product[] = [];
 
+  ngOnInit(): void {
+    this.products = [
+      {
+        id: 1,
+        title: 'Camisa Casual',
+        price: 29.99,
+        image: 'https://picsum.photos/640/640?random=1'
+      },
+      {
+        id: 2,
+        title: 'Vestido Elegante',
+        price: 49.99,
+        image: 'https://picsum.photos/640/640?random=2'
+      },
+      {
+        id: 3,
+        title: 'Hoodie Unisex',
+        price: 39.99,
+        image: 'https://picsum.photos/640/640?random=3'
+      },
+      {
+        id: 4,
+        title: 'Zapatillas Deportivas',
+        price: 59.99,
+        image: 'https://picsum.photos/640/640?random=4'
+      }
+    ];
+  }
 }
--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,14 +1,18 @@
-
-
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Importa CommonModule para usar *ngFor
 
 @Component({
   selector: 'app-cards',
   standalone: true,
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']  // Corrección aquí
+  styleUrls: ['./cards.component.css'],
+  imports: [CommonModule]  // Asegúrate de que CommonModule esté en la propiedad 'imports'
 })
 export class CardsComponent {
-  img ='https://picsum.photos/640/640?r='+ Math.random()
-
+  products = [
+    { title: 'Camisa Casual', price: 29.99, image: 'https://picsum.photos/640/640?random=1' },
+    { title: 'Vestido Elegante', price: 49.99, image: 'https://picsum.photos/640/640?random=2' },
+    { title: 'Hoodie Unisex', price: 39.99, image: 'https://picsum.photos/640/640?random=3' },
+    { title: 'Zapatillas Deportivas', price: 59.99, image: 'https://picsum.photos/640/640?random=4' }
+  ];
 }
