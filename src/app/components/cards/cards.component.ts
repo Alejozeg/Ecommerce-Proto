86ba12b75a13eb321ae4d9d8237980a4b4e1f918
detrujil1 <ingtrujillo6@gmail.com>
2024-10-22 19:02:21 -0500

    Revert "add inteface product"
    This reverts commit e68f721b528dbb0acfb8de24a2c1b6d260b12baa.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,18 +1,14 @@
+
+
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Importa CommonModule para usar *ngFor
 
 @Component({
   selector: 'app-cards',
   standalone: true,
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css'],
-  imports: [CommonModule]  // Asegúrate de que CommonModule esté en la propiedad 'imports'
+  styleUrls: ['./cards.component.css']  // Corrección aquí
 })
 export class CardsComponent {
-  products = [
-    { title: 'Camisa Casual', price: 29.99, image: 'https://picsum.photos/640/640?random=1' },
-    { title: 'Vestido Elegante', price: 49.99, image: 'https://picsum.photos/640/640?random=2' },
-    { title: 'Hoodie Unisex', price: 39.99, image: 'https://picsum.photos/640/640?random=3' },
-    { title: 'Zapatillas Deportivas', price: 59.99, image: 'https://picsum.photos/640/640?random=4' }
-  ];
+  img ='https://picsum.photos/640/640?r='+ Math.random()
+
 }
