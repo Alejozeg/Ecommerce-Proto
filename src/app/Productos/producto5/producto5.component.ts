191441114b1b6a55e46e6c608a05ba445bb66251
https://github.com/RichardWebMaster <richardtorrespinzon@gmail.com>
2024-10-25 11:26:42 -0500

    tarjeta detalle producto

--- src/app/Productos/producto5/producto5.component.ts
+++ src/app/Productos/producto5/producto5.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/api.service';
 import { Product } from '../../models/product.model';
+import { CarritoComponent } from '../../carrito/carrito.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-producto5',
   standalone: true,
-  imports: [],
+  imports: [CarritoComponent,CommonModule],
   templateUrl: './producto5.component.html',
   styleUrl: './producto5.component.css'
 })
@@ -25,6 +27,8 @@ export class Producto5Component {
   stock: '',
 
   }
+  cartsProducts: Product[] = [];
+
   constructor(private route: ActivatedRoute, private obtainProduct: ProductService) {}
 
   ngOnInit(): void {
@@ -33,6 +37,10 @@ export class Producto5Component {
       console.log(this.id);
     });
     this.getProduct()
+    const storedCart = localStorage.getItem('producto');
+    if (storedCart) {
+      this.cartsProducts = JSON.parse(storedCart);  // Restaura el carrito desde el localStorage
+    }
   }
   getProduct(){
     this.obtainProduct.getProductById(this.id).subscribe((data)=> {
@@ -40,4 +48,25 @@ export class Producto5Component {
       console.log(this.data)
     })
   }
+  @Input() producto!: Product;
+  // Método para añadir el producto al carrito
+// addToCart(product: Product) {
+
+//   localStorage.setItem("hola", "maicol");
+
+// }
+// / Método para añadir productos al carrito
+  addToCart(producto: Product) {
+    const productoExistente = this.cartsProducts.find(item => item._id === producto._id);
+
+    if (productoExistente) {
+      productoExistente.quantity! += 1;  // Incrementa la cantidad si el producto ya está en el carrito
+    } else {
+      producto.quantity = 1;  // Inicializa la cantidad si es un nuevo producto
+      this.cartsProducts.push(producto);
+    }
+
+    localStorage.setItem('producto', JSON.stringify(this.cartsProducts));
+  }
+
 }
