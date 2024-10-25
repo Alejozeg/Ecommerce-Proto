0da2a570945b0c4ddefff186c35755a999405341
https://github.com/RichardWebMaster <richardtorrespinzon@gmail.com>
2024-10-24 22:17:47 -0500

    color

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,LoginComponent,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onLogin(): void {
+    this.authService.login(this.email, this.password).subscribe(
+      (response: any) => {
+        console.log('Login exitoso', response);
+        // Lógica de redirección o almacenamiento de token
+      },
+      (error: any) => {
+        console.error('Error en el login', error);
+      }
+    );
+  }
 }
